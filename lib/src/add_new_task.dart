66a32bd693920e8626276f71a6b33799cf730e94
Rohan Mohite <rohan.mohite@gmail.com>
2026-06-30 23:47:32 +0530

    firebase Storage code added

--- lib/src/add_new_task.dart
+++ lib/src/add_new_task.dart
@@ -2,9 +2,12 @@ import 'dart:io';
 import 'package:auth_firebase/utils.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:flex_color_picker/flex_color_picker.dart';
+import 'package:dotted_border/dotted_border.dart';
+import 'package:uuid/uuid.dart';
 
 class AddNewTask extends StatefulWidget {
   const AddNewTask({super.key});
@@ -29,7 +32,13 @@ class _AddNewTaskState extends State<AddNewTask> {
 
   Future<void> uploadTaskToDb() async {
     try {
-      final data = await FirebaseFirestore.instance.collection('tasks').add({
+      final id = const Uuid().v4();
+      // final imagesRef = FirebaseStorage.instance.ref('images').child(id);
+      // final uploadTask = imagesRef.putFile(file!);
+      // final taskSnapshot = await uploadTask;
+      // final imageURL = await taskSnapshot.ref.getDownloadURL();
+      // print(imageURL);
+      await FirebaseFirestore.instance.collection('tasks').doc(id).set({
         'title': titleController.text.trim(),
         'description': descriptionController.text.trim(),
         'date': selectedDate,
@@ -37,7 +46,7 @@ class _AddNewTaskState extends State<AddNewTask> {
         'postedAt': FieldValue.serverTimestamp(),
         'color': rgbToHex(_selectedColor),
       });
-      print(data.id);
+      print(id);
     } catch (e) {
       print('Error uploading task: $e');
     }
@@ -75,36 +84,28 @@ class _AddNewTaskState extends State<AddNewTask> {
           child: Column(
             children: [
               // UNCOMMENT THIS in Firebase Storage section!
-
-              // GestureDetector(
-              //   onTap: () async {
-              //     final image = await selectImage();
-              //     setState(() {
-              //       file = image;
-              //     });
-              //   },
-              //   child: DottedBorder(
-              //     borderType: BorderType.RRect,
-              //     radius: const Radius.circular(10),
-              //     dashPattern: const [10, 4],
-              //     strokeCap: StrokeCap.round,
-              //     child: Container(
-              //       width: double.infinity,
-              //       height: 150,
-              //       decoration: BoxDecoration(
-              //         borderRadius: BorderRadius.circular(10),
-              //       ),
-              //       child: file != null
-              //           ? Image.file(file!)
-              //           : const Center(
-              //               child: Icon(
-              //                 Icons.camera_alt_outlined,
-              //                 size: 40,
-              //               ),
-              //             ),
-              //     ),
-              //   ),
-              // ),
+              GestureDetector(
+                onTap: () async {
+                  final image = await selectImage();
+                  setState(() {
+                    file = image;
+                  });
+                },
+                child: DottedBorder(
+                  child: Container(
+                    width: double.infinity,
+                    height: 150,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: file != null
+                        ? Image.file(file!)
+                        : const Center(
+                            child: Icon(Icons.camera_alt_outlined, size: 40),
+                          ),
+                  ),
+                ),
+              ),
               const SizedBox(height: 10),
               TextFormField(
                 controller: titleController,
